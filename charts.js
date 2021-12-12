12ab1ee3f66a4f4b670a02e7c16736e58154a4ef
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-12 17:13:32 -0500

    started harting features

--- charts.js
+++ charts.js
@@ -5,7 +5,7 @@ function init() {
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
-
+    selector.append("option").text("Select an ID")
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -58,30 +58,35 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    samples = data.samples
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    sample = samples.filter(x => x.id == sample)[0]
     //  5. Create a variable that holds the first sample in the array.
 
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    let otuids = sample.otu_ids
+    let outLables = sample.out_lables
+    let sampleValues = sample.sample_values
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
-
+    var yticks = otuids.slice(0,10).reverse()
+    var xticks = sampleValues.slice(0,10).reverse()
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = [{
+      x : xticks,
+      type : "bar",
+      orientation: 'h'
       
-    ];
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title : "Belly Button Colonies",
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar",barData,barLayout)
   });
 }
